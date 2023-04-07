a831e709cbb9a3f09b331ffe6377c015f94a3dd0
Omar Ochoa <omar@ochoawebdesign.com>
2023-04-06 21:48:38 -0700

    Sets WP_DEBUG and WP_DEBUG_LOG to true in WP-Config

--- wp-config.php
+++ wp-config.php
@@ -79,10 +79,13 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', false );
+define( 'WP_DEBUG', true );
 
 /* Add any custom values between this line and the "stop editing" line. */
 
+
+// Enable logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', true );
 
 
 /* That's all, stop editing! Happy publishing. */
